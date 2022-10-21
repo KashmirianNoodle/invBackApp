26a552147115eeb211fec589c67e9a139aed0464
KashmirianNoodle <72066697+KashmirianNoodle@users.noreply.github.com>
2022-10-21 09:39:20 +0530

    changed swagger def

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -4,7 +4,7 @@ const config = require('../config/config');
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-boilerplate API documentation',
+    title: 'api documentation',
     version,
     license: {
       name: 'MIT',
@@ -13,7 +13,7 @@ const swaggerDef = {
   },
   servers: [
     {
-      url: `http://localhost:${config.port}/v1`,
+      url: `https://floating-forest-60538.herokuapp.com/v1/docs/#/`,
     },
   ],
 };
